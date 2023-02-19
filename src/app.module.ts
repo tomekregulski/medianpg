1ea1916e4922429497afafa6eb43483abc3a4fe7
Tomek Regulski <tomek.regulski@gmail.com>
2023-02-19 08:11:44 -0500

    create events and locations

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
 import { RagasModule } from './ragas/ragas.module';
+import { LocationsModule } from './locations/locations.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule, RagasModule],
+  imports: [PrismaModule, ArticlesModule, RagasModule, LocationsModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
